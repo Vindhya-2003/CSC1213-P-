42e0dd4fc23ac8832b99086b619a761fee95d871
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-15 21:06:59 +0530

    Upload Ex1

---
+++ Day1/Class Work/Ex01/FirstJavaProgramme.java
@@ -0,0 +1,9 @@
+class FirstJavaProgramme
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My First Programme in Java");
+		System.out.println("Hello Java Developers!!!");
+	}
+}
+		
