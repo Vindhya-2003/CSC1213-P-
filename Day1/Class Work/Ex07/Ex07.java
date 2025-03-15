fc9622d15b5e4795c10b2c3734c55cfb2df62d79
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-15 23:05:10 +0530

    Upload Ex07

---
+++ Day1/Class Work/Ex07/Ex07.java
@@ -0,0 +1,14 @@
+class Ex07
+{
+	public static void main(String args[])
+	{
+		int x=20;
+		int y=10;
+		
+		System.out.println("Addition is:"+(x+y));
+		System.out.println("Substraction is:"+(x-y));
+		System.out.println("Mutiplication is:"+(x*y));
+		System.out.println("Divition is:"+(x/y));
+		System.out.println("Modulus is:"+(x%y));
+	}
+}
