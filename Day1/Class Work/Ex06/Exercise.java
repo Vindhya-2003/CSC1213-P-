1b3fcb436ffc51174abc28e253475b71fdeb7736
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-15 22:46:22 +0530

    Upload Ex06

---
+++ Day1/Class Work/Ex06/Exercise.java
@@ -0,0 +1,20 @@
+class Exercise
+{
+	public static void main(String args[])
+	{
+		char cl='g';
+		System.out.println("The character is :"+cl);
+		
+		int int1=(int)cl;
+		System.out.println("The character is :"+int1);
+		
+	double dl=10.09;
+	System.out.println("The value is :"+dl);
+	
+	int int2=(int)dl;
+	System.out.println("The value is :"+int2);
+	
+	double d2=int1;
+	System.out.println("The value is :"+d2);
+	}
+}
