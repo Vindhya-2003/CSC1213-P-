7997dc157823c8005036c07bb330e5d025e183da
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-15 21:26:06 +0530

    Upload Ex02

---
+++ Day1/Class Work/Ex02/Ex02.java
@@ -0,0 +1,24 @@
+class Ex02
+{
+	public static void main(String[] args)
+	{
+		String fac="FAS";
+		int n=3;
+		double p=10.5;
+		int m=5;
+		char wl='&';
+		boolean isTH=true;
+		
+		System.out.println(fac);
+		System.out.println(n);
+		System.out.println(p);
+		System.out.println(m);
+		System.out.println(wl);
+		System.out.println(isTH);
+		
+		System.out.println("My Faculty is"+fac);
+		System.out.println("The result is"+(n+m));
+		System.out.println("The result2 is"+(p+m));
+	}
+}
+		
