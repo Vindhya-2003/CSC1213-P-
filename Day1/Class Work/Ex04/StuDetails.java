709fcb6058b2169db6b94a9c52048c060fba126f
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-15 22:15:51 +0530

    Upload Ex04

---
+++ Day1/Class Work/Ex04/StuDetails.java
@@ -0,0 +1,18 @@
+class StuDetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Vindhya Sadamini");
+		System.out.println("Registration Number: 2022/ASP/72");
+		System.out.println("Degree Programm:BSc.ComputerScience");
+		
+		String Name="Vindhya Sadamini";
+		String RegNo="2022/ASP/72";
+		String Degree="BSc.ComputerScience";
+		
+	System.out.println("Full Name:"+Name);
+	System.out.println("Registration Number:"+RegNo);
+	System.out.println("Degree Programm:"+Degree);
+	}
+}
+		
