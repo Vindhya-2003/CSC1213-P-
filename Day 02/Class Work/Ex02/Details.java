09e9f6a355d941ed5edb550a0125bccf77396c4d
Vindhya-2003 <vindhyasandamini@gmail.com>
2025-03-22 00:54:29 +0530

    Upload Ex02

---
+++ Day 02/Class Work/Ex02/Details.java
@@ -0,0 +1,15 @@
+class Details
+{
+	public static void main(String args[])
+	{
+		String Name,Address,DOB,Sex;
+		int Age;
+		Name=args[0];
+		Address=args[1];
+		DOB=args[2];
+		Sex=args[3];
+		Age=Integer.parseInt(args[4]);
+		
+		System.out.println("Name:"+Name+"\n"+"Address:"+Address+"\n"+"Date of Birthday:"+DOB+"\n"+"Sex:"+Sex+"\n"+"Age:"+Age);
+	}
+}
